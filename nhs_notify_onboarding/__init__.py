7135b576cf9ac9a324111ba2d3d22c1461d20ebe
lapenna-bjss <luca.lapenna@bjss.com>
2026-04-01 12:43:36 +0100

    CCM-14566: Make the repo a python package

---
+++ nhs_notify_onboarding/__init__.py
@@ -0,0 +1,30 @@
+import importlib.resources
+from typing import Any
+
+import jsonschema
+import yaml
+
+ValidationError = jsonschema.ValidationError
+
+_SCHEMA = None
+
+
+def _get_schema() -> dict:
+    global _SCHEMA
+    if _SCHEMA is None:
+        text = (
+            importlib.resources.files("nhs_notify_onboarding")
+            .joinpath("schema.yaml")
+            .read_text(encoding="utf-8")
+        )
+        _SCHEMA = yaml.safe_load(text)
+    return _SCHEMA
+
+
+def validate(instance: Any) -> None:
+    """Validate a DocumentReference dict against the NHS Notify schema.
+
+    Raises:
+        jsonschema.ValidationError: if the payload is invalid.
+    """
+    jsonschema.validate(instance=instance, schema=_get_schema())
